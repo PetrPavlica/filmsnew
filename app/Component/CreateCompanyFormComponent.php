7ce164be0a61871a2a4a826a7b998f24c572bb6e
crook <crook@DESKTOP-FPIPLHD>
2022-04-01 10:09:27 -0700

    last update

--- app/Component/CreateCompanyFormComponent.php
+++ app/Component/CreateCompanyFormComponent.php
@@ -8,6 +8,8 @@ class CreateCompanyFormComponent extends Nette\Application\UI\Control
     
     public $onCreateCompanyFormSave;
     
+    public $ares_firma_fin;
+    
     public function __construct() 
     {
 
@@ -15,8 +17,69 @@ class CreateCompanyFormComponent extends Nette\Application\UI\Control
     
     public function handleAres($ico) 
     {
-       bdump($ico); 
-    }
+        $ares_ico_fin = "";
+        $ares_dic_fin = "";
+        $ares_firma_fin = "";
+        $ares_ulice_fin = "";
+        $ares_cp1_fin   = "";
+        $ares_cp2_fin   = "";
+        $ares_mesto_fin = "";
+        $ares_psc_fin   = "";
+        $ares_stav_fin = "";
+        $file = @file_get_contents("http://wwwinfo.mfcr.cz/cgi-bin/ares/darv_bas.cgi?ico=".$ico);
+
+        if($file)
+          {
+            $xml = @simplexml_load_string($file);
+          }
+
+        if($xml) 
+          {
+            $ns = $xml->getDocNamespaces();
+            $data = $xml->children($ns['are']);
+            $el = $data->children($ns['D'])->VBAS;
+
+            if (strval($el->ICO) == $ico) 
+              {
+                $ares_ico_fin = strval($el->ICO);
+                $ares_dic_fin = strval($el->DIC);
+                $ares_firma_fin = strval($el->OF);
+                $ares_ulice_fin = strval($el->AA->NU);
+                $ares_cp1_fin   = strval($el->AA->CD);
+                $ares_cp2_fin   = strval($el->AA->CO);
+                
+                if($ares_cp2_fin != "")
+                { 
+                    $ares_cp_fin = $ares_cp1_fin."/".$ares_cp2_fin; 
+                }
+                else
+                { 
+                    $ares_cp_fin = $ares_cp1_fin; 
+                
+                }
+                
+                $ares_mesto_fin = strval($el->AA->N);
+                $ares_psc_fin   = strval($el->AA->PSC);
+                $ares_stav_fin = 1;
+              } 
+            else
+              {
+                $ares_stav_fin  = 'IČO firmy nebylo nalezeno';
+              } 
+          }
+        else
+          {
+            $ares_stav_fin  = 'Databáze ARES není dostupná';
+          }
+          
+          $form = $this->getComponent('createCompanyForm');
+          $form->setDefaults(['dic'=>$ares_dic_fin,
+                              'company_name'=>$ares_firma_fin,
+                              'company_adress'=>$ares_cp1_fin
+                             ]);
+         $this->redrawControl('snippet-createCompanyForm-form');
+         
+          }
     
      public function createComponentCreateCompanyForm() 
 
@@ -59,6 +122,7 @@ class CreateCompanyFormComponent extends Nette\Application\UI\Control
     }
     
     public function render(){
+       
        $this->template->render(__DIR__ .'/createcompanyform.latte');
     }
 }
