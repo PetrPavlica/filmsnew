f80662a63fb3b5853fc262695eec0d3e14515b3b
petrp <petrp@DESKTOP-GAB3FJB>
2022-04-04 06:53:04 +0200

    final versin 1

--- app/Component/CreateCompanyFormComponent.php
+++ app/Component/CreateCompanyFormComponent.php
@@ -1,11 +1,9 @@
-<?php
-declare(strict_types=1);
+<?php declare(strict_types = 1);
 
 use Nette\Application\UI\Form;
 
-class CreateCompanyFormComponent extends Nette\Application\UI\Control
+final class CreateCompanyFormComponent extends Nette\Application\UI\Control
 {
-    
     public $onCreateCompanyFormSave;
     
     public $ares_firma_fin;
@@ -17,21 +15,21 @@ class CreateCompanyFormComponent extends Nette\Application\UI\Control
     
     public function handleAres($ico) 
     {
-        $ares_ico_fin = "";
-        $ares_dic_fin = "";
-        $ares_firma_fin = "";
-        $ares_ulice_fin = "";
-        $ares_cp1_fin   = "";
-        $ares_cp2_fin   = "";
-        $ares_mesto_fin = "";
-        $ares_psc_fin   = "";
-        $ares_stav_fin = "";
-        $file = @file_get_contents("http://wwwinfo.mfcr.cz/cgi-bin/ares/darv_bas.cgi?ico=".$ico);
+        $ares_dic = "";
+        $ares_company = "";
+        $ares_street = "";
+        $ares_housenum   = "";
+        $ares_housenum1   = "";
+        $ares_housenum2   = "";
+        $ares_city = "";
+        $ares_citycode   = "";
+        $ares_stav = "";
+        $file = @file_get_contents("http://wwwinfo.mfcr.cz/cgi-bin/ares/darv_bas.cgi?ico=" . $ico);
 
         if($file)
           {
             $xml = @simplexml_load_string($file);
-          }
+        }
 
         if($xml) 
           {
@@ -39,97 +37,103 @@ class CreateCompanyFormComponent extends Nette\Application\UI\Control
             $data = $xml->children($ns['are']);
             $el = $data->children($ns['D'])->VBAS;
 
-            if (strval($el->ICO) == $ico) 
+            if (strval($el->ICO) === $ico) 
               {
-                $ares_ico_fin = strval($el->ICO);
-                $ares_dic_fin = strval($el->DIC);
-                $ares_firma_fin = strval($el->OF);
-                $ares_ulice_fin = strval($el->AA->NU);
-                $ares_cp1_fin   = strval($el->AA->CD);
-                $ares_cp2_fin   = strval($el->AA->CO);
-                
-                if($ares_cp2_fin != "")
+                $ares_dic = strval($el->DIC);
+
+                if("" === $ares_dic)
                 { 
-                    $ares_cp_fin = $ares_cp1_fin."/".$ares_cp2_fin; 
+                    $ares_dic = 'neni'; 
+                }
+                
+                $ares_company = strval($el->OF);
+                $ares_street = strval($el->AA->NU);
+                $ares_housenum1 = strval($el->AA->CD);
+                $ares_housenum2 = strval($el->AA->CO);
+                
+                if("" !== $ares_housenum2)
+                { 
+                    $ares_housenum = $ares_housenum1 . "/" . $ares_housenum2; 
                 }
                 else
                 { 
-                    $ares_cp_fin = $ares_cp1_fin; 
+                    $ares_housenum = $ares_housenum1; 
                 
                 }
                 
-                $ares_mesto_fin = strval($el->AA->N);
-                $ares_psc_fin   = strval($el->AA->PSC);
-                $ares_stav_fin = 1;
-              } 
+                $ares_city = strval($el->AA->N);
+                $ares_citycode   = strval($el->AA->PSC);
+                $ares_stav = 1;
+            } 
             else
               {
-                $ares_stav_fin  = 'IČO firmy nebylo nalezeno';
-              } 
-          }
+                $ares_stav  = 'IČO firmy nebylo nalezeno';
+            } 
+        }
         else
           {
-            $ares_stav_fin  = 'Databáze ARES není dostupná';
-          }
-          
-          $form = $this->getComponent('createCompanyForm');
-          $form->setDefaults(['dic'=>$ares_dic_fin,
-                              'company_name'=>$ares_firma_fin,
-                              'company_adress'=>$ares_cp1_fin
-                             ]);
-         $this->redrawControl('snippet-createCompanyForm-form');
+            $ares_stav  = 'Databáze ARES není dostupná';
+        }
          
-          }
+            $this->presenter->sendJson(['dic' => $ares_dic,
+                             'company_name' => $ares_company,
+                             'company_adress' => $ares_street . ' ' . $ares_housenum . ', ' . $ares_city . ' ' . $ares_citycode,
+                             'ares' => $ares_stav,   
+                            ]);
+        
+    }
     
-     public function createComponentCreateCompanyForm() 
+    public function createComponentCreateCompanyForm() 
 
     {
-        $form = new Form;
+        $form = new Form();
         
         $form->addText('ico','IČO:')
-             ->setRequired('Zadejte IČO')
-              ->setHtmlAttribute('id', 'ico')
-             ->setHtmlAttribute('onchange', 'ares()');
+            ->setRequired('Zadejte IČO')
+            ->setHtmlAttribute('id', 'ico')
+            ->setHtmlAttribute('onchange', 'ares()');
         
         $form->addText('dic','DIČ:')
-             ->setRequired('Zadejte DIČ');
+            ->setRequired('Zadejte DIČ')
+            ->setHtmlAttribute('id', 'dic');
                 
         $form->addText('company_name','Název firmy:')
-            ->setRequired('Zadejte název firmy');
+            ->setRequired('Zadejte název firmy')
+            ->setHtmlAttribute('id', 'company_name');
           
         $form->addText('company_adress','Adresa firmy:')
-            ->setRequired('Zadejte adresu firmy');
+            ->setRequired('Zadejte adresu firmy')
+            ->setHtmlAttribute('id', 'company_adress');
         
-        $form->addText('company_person_name','Adresa firmy:')
-                        ->setRequired('Zadejte jméno');
+        $form->addText('company_person_name','Jméno statutárního zástupce:')
+            ->setRequired('Zadejte jméno');
         
-        $form->addText('company_person_email','Adresa firmy:');
+        $form->addText('company_person_email','Email statutárního zástupce:');
          
         $form->addSubmit('send', 'Uložit')
-        ->setAttribute('class', 'btn btn-info btn-sm');   
+            ->setAttribute('class', 'btn btn-info btn-sm');   
 
        
         $form->onSuccess[] = [$this, 'processForm'];
+
         return $form;
     }
     
     public function processForm($form)
     {
-             
-        $data = $form->getValues();
-        $this->presenter->flashMessage('Uloženo');
+        $data = $form->getValues(true);
         $this->onCreateCompanyFormSave($data);
     }
     
-    public function render(){
-       
-       $this->template->render(__DIR__ .'/createcompanyform.latte');
+    public function render()
+    {
+        $this->template->render(__DIR__ . '/createcompanyform.latte');
     }
 }
 
 interface ICreateCompanyFormComponent
 {
     /** @return \CreateCompanyFormComponent */
-    function create();
+    public function create();
 }
 
--- app/Presenters/CompanyPresenter.php
+++ app/Presenters/CompanyPresenter.php
@@ -1,6 +1,4 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types = 1);
 
 namespace App\Presenters;
 
@@ -9,15 +7,37 @@ use Nette;
 
 final class CompanyPresenter extends Nette\Application\UI\Presenter
 {
-   /** @var \ICreateCompanyFormComponent @inject */
+    /** @var \ICreateCompanyFormComponent @inject */
     public $createCompanyFormControl;
     
-    protected function createComponentCreateCompanyForm(): \CreateCompanyFormComponent {
-        
+    protected function createComponentCreateCompanyForm(): \CreateCompanyFormComponent
+    {
         $component = $this->createCompanyFormControl->create();
         $component->onCreateCompanyFormSave[] = function ($data) {
-            
-		};
+            $this->redirect('Company:default',$data['ico'],
+                                              $data['dic'],
+                                              $data['company_name'],
+                                              $data['company_adress'],
+                                              $data['company_person_name'],
+                                              $data['company_person_email']);
+        };
+
         return $component;
+    }
+    
+    public function renderDefault($ico = null,
+                                  $dic = null,
+                                  $company_name = null, 
+                                  $company_adress=null,
+                                  $company_person_name=null,
+                                  $company_person_email=null
+                                 ) 
+    {
+        $this->template->ico = $ico;
+        $this->template->dic = $dic;
+        $this->template->company_name = $company_name;
+        $this->template->company_adress = $company_adress;
+        $this->template->company_person_name = $company_person_name;
+        $this->template->company_person_email = $company_person_email;
     }
 }
